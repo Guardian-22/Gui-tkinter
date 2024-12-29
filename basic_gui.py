0f602657f99e4f41d936670a817a195fd567377e
Guardian-22 <kshitijlalge@gmail.com>
2024-12-29 22:53:55 +0530

    Base of tkinter gui

---
+++ basic_gui.py
@@ -0,0 +1,12 @@
+from  tkinter import *
+"""
+Creates the main application window (often called the "root window").
+Tk() is a class in the tkinter module and is the entry point for any Tkinter application.
+"""
+root=Tk()
+
+"""
+Starts the event loop, which waits for user interactions like button clicks or keypresses.
+This loop keeps the GUI running until the user closes the window.
+"""
+root.mainloop()
